445f5661359aa85a4af8b3d9ccf0d925a32a657f
Yo Sev <severinsiebertz@googlemail.com>
2025-05-19 12:20:30 +0200

    fix: remove trailing spaces in urls

--- Casks/ffmate.rb
+++ Casks/ffmate.rb
@@ -3,12 +3,12 @@ cask "ffmate" do
 
   on_intel do
     sha256 "04350eddd4a070c24e7516be7d7fd895484296d774eaad123f6c00e5c2732fee"
-    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate_Darwin_x86_64.tar.gz "
+    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate_Darwin_x86_64.tar.gz"
   end
 
   on_arm do
     sha256 "a05dc04ffdd39bfbd7628844d3c1157a482fa2b469f03abf00a2a432db2fa939"
-    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate_Darwin_arm64.tar.gz "
+    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate_Darwin_arm64.tar.gz"
   end
 
   name "ffmate"
