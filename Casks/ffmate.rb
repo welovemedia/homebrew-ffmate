8e93ecd47489f4faef763f0e80ad2ead5f6ea9a2
Yo Sev <severinsiebertz@googlemail.com>
2025-06-06 16:29:33 +0200

    fix: urls to releases

--- Casks/ffmate.rb
+++ Casks/ffmate.rb
@@ -3,12 +3,12 @@ cask "ffmate" do
 
   on_intel do
     sha256 "efa92581d27c8145e074827f4e51b6314ff18cb0191774d673500c8fc3c536bf"
-    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate_Darwin_x86_64.tar.gz"
+    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate_darwin_amd64.tar.gz"
   end
 
   on_arm do
     sha256 "e29342c07e5d1a1b798fae5ac7a0f0f2642c8fc8c4cec6fd6800924ad976a14a"
-    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate_Darwin_arm64.tar.gz"
+    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate_darwin_arm64.tar.gz"
   end
 
   name "ffmate"
