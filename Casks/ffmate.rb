5e18200dde1d1284d4d8ee259531936ab2b58ec8
Yo Sev <severinsiebertz@googlemail.com>
2025-06-06 16:35:40 +0200

    fix: remove tar suffix from file url

--- Casks/ffmate.rb
+++ Casks/ffmate.rb
@@ -3,12 +3,12 @@ cask "ffmate" do
 
   on_intel do
     sha256 "a490cf57c317d6f763463f38b975ad72c23b45d46e2e8dc3bcf881973bae5fb1"
-    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate-darwin-amd64.tar.gz"
+    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate-darwin-amd64.gz"
   end
 
   on_arm do
     sha256 "62e6ecc77df199ccad4aa7fb5dd1934fdc4e03eee729903479e1c5c71e1e696f"
-    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate-darwin-arm64.tar.gz"
+    url "https://github.com/welovemedia/ffmate/releases/download/#{version}/ffmate-darwin-arm64.gz"
   end
 
   name "ffmate"
